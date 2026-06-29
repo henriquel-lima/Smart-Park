9e65fe86e0b0bebfe72f1a8e0c7c54f12015f2df
henriquel-lima <henrique.l@aln.senaicimatec.edu.br>
2026-06-29 15:58:37 -0300

    Delete calibragem.ino

--- calibragem.ino
+++
@@ -1,9 +0,0 @@
-void calibrarSensor(){
-  float soma = 0;
-  for(int i = 0; i<10; i++){
-    float d = sensorVagaA1.lerDistancia();
-    soma += d;
-    delay(500);
-  }
-  alturaSensorChao = soma / 10;
-}
