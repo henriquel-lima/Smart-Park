4973ebe4ff60f55dc11902ec012c4656b91f182d
henriquel-lima <henrique.l@aln.senaicimatec.edu.br>
2026-06-30 11:49:37 -0300

    Add secrets.h for Wi-Fi and server configuration

---
+++ ESP32_Gateway/secrets.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define WIFI_SSID "NOME DA REDE"       // ATENÇÃO: Substitua pelo nome (SSID) da sua rede Wi-Fi.
+#define WIFI_PASSWORD "SENHA DA REDE"  // ATENÇÃO: Substitua pela senha correspondente à sua rede Wi-Fi.
+#define SERVER_URL "http://000.000.0.000:1880/vaga" // ATENÇÃO: Substitua o endereço IPv4 "000.000.0.000" pelo endereço correspondente à sua rede local.
